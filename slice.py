d7e8531d73d0efdb566b42966dd8e473d6f2f81c
Ryan Gomba <ryangomba@fb.com>
2014-10-11 18:54:32 -0700

    Attempt at diffing

--- slice.py
+++ slice.py
@@ -4,7 +4,99 @@ import subprocess
 import shutil
 import json
 import re
+import time
+import hashlib
 from distutils.spawn import find_executable
+
+################################################################################
+# HELPERS
+################################################################################
+
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def create_empty_directory(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+    os.makedirs(path)
+
+def json_dict_for_file_path(file_path):
+    if os.path.exists(file_path):
+        return json.load(open(file_path, 'r'))
+    return None
+
+def write_dict_to_file_path(file_path, info):
+    json_file = open(file_path, 'w+')
+    json_file.write(json.dumps(info))
+    json_file.close()
+
+################################################################################
+# MODELS
+################################################################################
+
+class AssetGroup(object):
+    def __init__(self, name):
+        self.name = name
+        self.assets = []
+
+    def generate_hash(self):
+        md5 = hashlib.md5()
+        for asset in self.assets:
+            asset_data = open(asset.path, 'rb').read()
+            md5.update(asset_data)
+        self.md5 = md5.hexdigest()
+        return self.md5
+
+    @property
+    def imageset_directory(self):
+        imageset_name = self.name + ".imageset"
+        return os.path.join(OUTPUT_DIR, imageset_name)
+
+    @property
+    def imageset_info_file_path(self):
+        return os.path.join(self.imageset_directory, 'Contents.json')
+
+    @property
+    def info(self):
+        info = {
+            'images': [],
+            'info': {
+                'version': self.md5,
+                'author': 'Slicer',
+            },
+        }
+        for asset in self.assets:
+            info['images'].append(asset.info)
+        return info
+
+class Asset(object):
+    def __init__(self, path):
+        self.path = path
+        self.filename = path.rpartition("/")[2]
+        regex_match = re.search("(\\w+)@?(.*?)\\.(.+)", filename, re.S)
+        self.group_name = regex_match.group(1)
+        scale = regex_match.group(2)
+        if len(scale) == 0:
+            scale = '1x'
+        self.scale = scale
+        self.extension = regex_match.group(3)
+
+    @property
+    def info(self):
+        return {
+            'idiom': 'universal',
+            'scale': self.scale,
+            'filename': self.filename,
+        }
+
+################################################################################
+
+# start timer
+
+start_time = time.time()
+
+# parse arguments
 
 if not len(sys.argv) == 3:
     print "Proper usage: python slice.py [.sketch file] [.xcassets file]"
@@ -26,15 +118,19 @@ if not ASSETS_FILE.endswith('.xcassets'):
     print "Output file is expected to be a .xcassets bundle"
     exit(1)
 
+EXPORT_DIR = os.path.join(os.path.dirname(INPUT_FILE), 'tmp_exported_slices')
 OUTPUT_DIR = os.path.join(ASSETS_FILE, 'Slices')
+OUTPUT_INFO_PATH = os.path.join(OUTPUT_DIR, 'info.json')
 
-# clear the output directory
+# check to see if the sketch file has been modified
 
-print "Creating output directory"
-
-if os.path.exists(OUTPUT_DIR):
-    shutil.rmtree(OUTPUT_DIR)
-os.makedirs(OUTPUT_DIR)
+export_info = json_dict_for_file_path(OUTPUT_INFO_PATH)
+if export_info:
+    export_timestamp = export_info.get('timestamp')
+    input_file_modification_timestamp = os.path.getmtime(INPUT_FILE)
+    if export_timestamp > input_file_modification_timestamp:
+        print 'Sketch file has not been modified; skipping export'
+        exit(0)
 
 # export slices
 
@@ -50,71 +146,80 @@ if sketchtool_executable is None:
     print 'Please install sketchtool'
     exit(1)
 
-print "Exporting slices from", INPUT_FILE, "to", ASSETS_FILE
+print "Creating temporary export directory"
+create_empty_directory(EXPORT_DIR)
+
+print "Exporting slices from", INPUT_FILE, "to temporary directory", EXPORT_DIR
 
 subprocess.call([
     sketchtool_executable,
     'export',
     'slices',
     INPUT_FILE,
-    '--output=' + OUTPUT_DIR,
+    '--output=' + EXPORT_DIR,
 ])
 
-# define the asset model
-
-class Asset(object):
-    def __init__(self, path):
-        self.path = path
-        self.filename = path.rpartition("/")[2]
-        regex_match = re.search("(\\w+)@?(.*?)\\.(.+)", filename, re.S)
-        self.name = regex_match.group(1)
-        scale = regex_match.group(2)
-        if len(scale) == 0:
-            scale = '1x'
-        self.scale = scale
-        self.extension = regex_match.group(3)
-
-    def info(self):
-        return {
-            'idiom': 'universal',
-            'scale': self.scale,
-            'filename': self.filename,
-        }
-
-# group slices into sets of assets
+# group into sets of assets
 
 print "Grouping assets"
 
-slices = {}
-for filename in os.listdir(OUTPUT_DIR):
-    asset_path = os.path.join(OUTPUT_DIR, filename)
+asset_groups = {}
+for filename in os.listdir(EXPORT_DIR):
+    asset_path = os.path.join(EXPORT_DIR, filename)
     asset = Asset(asset_path)
-    assets_for_slice_name = slices.get(asset.name, [])
-    assets_for_slice_name.append(asset)
-    slices[asset.name] = assets_for_slice_name
+    asset_group = asset_groups.get(asset.group_name)
+    if asset_group is None:
+        asset_group = AssetGroup(asset.group_name)
+        asset_groups[asset.group_name] = asset_group
+    asset_group.assets.append(asset)
+    asset_group.generate_hash()
+
+# determine which assets have changed
+
+modified_asset_groups = []
+for asset_group in asset_groups.values():
+    info_file_path = asset_group.imageset_info_file_path
+    if os.path.exists(info_file_path):
+        info = json.load(open(info_file_path, 'r'))
+        if info:
+            group_hash = info['info']['version']
+            if group_hash == asset_group.md5:
+                break
+    modified_asset_groups.append(asset_group)
+
+if len(modified_asset_groups) == 0:
+    print 'All assets up to date'
+else:
+    print 'Detected %d modified asset groups' % len(modified_asset_groups)
+    print "Generating imagesets"
 
 # move assets into imagesets
 
-print "Generating imagesets"
+print "Creating output directory"
+create_directory(OUTPUT_DIR)
 
-for slice_name, assets in slices.iteritems():
-    imageset_name = slice_name + ".imageset"
-    imageset_directory = os.path.join(OUTPUT_DIR, imageset_name)
-    os.makedirs(imageset_directory)
-    imageset_info = {
-        'images': [],
-        'info': {
-            'version': 1,
-            'author': 'xcode',
-        },
-    }
-    for asset in assets:
+for asset_group in modified_asset_groups:
+    # create the imageset
+    imageset_directory = os.path.join(OUTPUT_DIR, asset_group.name + '.imageset')
+    create_empty_directory(imageset_directory)
+    # place the assets in the imageset
+    for asset in asset_group.assets:
         shutil.move(asset.path, imageset_directory)
-        imageset_info['images'].append(asset.info())
+    # write the imageset info
     imageset_info_file_path = os.path.join(imageset_directory, 'Contents.json')
-    imageset_info_file = open(imageset_info_file_path, 'w+')
-    imageset_info_file.write(json.dumps(imageset_info))
-    imageset_info_file.close()
+    write_dict_to_file_path(imageset_info_file_path, asset_group.info)
 
-print "Slices added to ", ASSETS_FILE
+# save some export info
+
+write_dict_to_file_path(OUTPUT_INFO_PATH, {'timestamp': time.time()})
+
+# delete the temporary export directory
+
+print 'Cleaning up'
+shutil.rmtree(EXPORT_DIR)
+
+# print the time
+
+time_elapsed = time.time() - start_time
+print 'Finished in %f seconds' % time_elapsed
 
